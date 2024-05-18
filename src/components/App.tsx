183422317af2e8d389c229ada0f8b0070e187d6e
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-18 14:52:19 -0700

    debounce search requests

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Background from "./Background";
 import Container from "./Container";
 import Footer from "./Footer";
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,10 +1,24 @@
 import { useEffect, useState } from "react";
 import { JobDescription, JobItem } from "./types";
 import { BASE_API_URL } from "./constants";
+// import { useDebounceValue } from "usehooks-ts";
 
 const MAX_JOB_ITEMS = 7;
+const MAX_SEARCH_DEBOUNCE_TIME_MS = 250;
 
 export const useFetchJobItems = (searchText: string) => {
+  // const [debouncedSearchText, debounce] = useDebounceValue(searchText, MAX_SEARCH_DEBOUNCE_TIME_MS);
+  const debouncedSearchText = useDebounce(
+    searchText,
+    MAX_SEARCH_DEBOUNCE_TIME_MS
+  );
+
+  // Alternative way to debounce search text using usehooks-ts library
+  // useEffect(() => {
+  //   if (searchText === debouncedSearchText) return;
+  //   debounce(searchText);
+  // }, [searchText, debouncedSearchText, debounce]);
+
   const [jobItems, setJobItems] = useState<JobItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -13,11 +27,19 @@ export const useFetchJobItems = (searchText: string) => {
   const totalJobItems = jobItems.length;
 
   useEffect(() => {
-    if (!searchText) return;
+    if (!debouncedSearchText) return;
     setIsLoading(true);
+    // Flexible but less robust way to construct URL
+    // const url = `${BASE_API_URL}?search=${debouncedSearchText}`;
+    // Preferred way to construct URL for following reasons:
+    // - URLSearchParams automatically encodes special characters
+    // - URL object provides convenient methods to modify URL
+    // - URL object provides a clean way to construct and read URL
+    // - URL object provides a clean way to append query parameters
     const url = new URL(BASE_API_URL);
     const params = new URLSearchParams(url.search);
-    params.set("search", searchText);
+    params.set("search", debouncedSearchText);
+    url.search = params.toString();
 
     // Mock backend server will always return random amount of data
     const fetchData = async () => {
@@ -33,7 +55,7 @@ export const useFetchJobItems = (searchText: string) => {
     };
 
     fetchData();
-  }, [searchText]);
+  }, [debouncedSearchText]);
 
   return { jobItemsSliced, isLoading, totalJobItems } as const;
 };
@@ -86,4 +108,20 @@ export const useFetchJobItemById = (jobId: number | null) => {
   }, [jobId]);
 
   return [job, isLoading] as const;
+};
+
+// Use generic type to make useDebounce hook more flexible
+// <T> let's TypeScript know that we are using a generic type
+// Generics establishes a relationship between:
+// - the type of the value passed to the hook
+// - the type of the value returned by the hook
+export const useDebounce = <T>(value: T, time: number = 500): T => {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => setDebouncedValue(value), time);
+    return () => clearTimeout(timerId);
+  }, [value, time]);
+
+  return debouncedValue;
 };
