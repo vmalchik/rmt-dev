8451a454eb9237eb1d71c141c4c2f275adbeccc9
Victor Malchikov <victor.malchikov@netwitness.com>
2024-05-21 20:51:46 -0700

    place popover into portal

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,6 +1,7 @@
 import { forwardRef } from "react";
 import { useBookmarksContext } from "../lib/hooks";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 type BookmarksPopoverProps = unknown; // Component does not receive any props
 
@@ -8,10 +9,13 @@ const BookmarksPopover = forwardRef<HTMLDivElement, BookmarksPopoverProps>(
   (_, ref) => {
     const { bookmarkedJobItems, isLoading } = useBookmarksContext();
 
-    return (
+    // place popover into its own space to prevent popover from being impacted by surrounding elements
+    // eg. z-index impacting display of popover
+    return createPortal(
       <div ref={ref} className="bookmarks-popover">
         <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-      </div>
+      </div>,
+      document.body
     );
   }
 );
